25efc520dbd7a84028a76ed9f3e39c99898ee635
Felipe <balcaam@gmail.com>
2020-11-26 16:13:27 -0300

    second

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,14 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  def new
+    @task = Task.new
+  end
   def show
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
+  end
+  def create
+    @task = Task.new(task_params)
+    @task.save
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_174149) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.boolean "completed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_201_126_174_149) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.text 'details'
+    t.boolean 'completed'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
