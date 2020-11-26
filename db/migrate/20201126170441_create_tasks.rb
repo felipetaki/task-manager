04985c3f419b8d19204359defe07e7e8729bb5ca
Felipe <balcaam@gmail.com>
2020-11-26 14:05:38 -0300

    submit

---
+++ db/migrate/20201126170441_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :default
+      t.false :completed
+
+      t.timestamps
+    end
+  end
+end
