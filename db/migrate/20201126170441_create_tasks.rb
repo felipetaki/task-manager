a42a8ff30dacd0f42a4e4d9585ad0853247bae9d
Felipe <balcaam@gmail.com>
2020-11-26 14:34:38 -0300

    problem

--- db/migrate/20201126170441_create_tasks.rb
+++ db/migrate/20201126170441_create_tasks.rb
@@ -4,8 +4,8 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :default
-      t.false :completed
+      t.text :details
+      t.boolean :completed
 
       t.timestamps
     end
